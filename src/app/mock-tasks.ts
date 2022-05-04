1994d4474395824f78d84d1a8cf2c889e5bf5f12
Chikondi Luka <65278289+XxCLxX@users.noreply.github.com>
2022-05-04 20:08:52 +0700

    Initial commit testing
    testing

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "Feb 15 at 2:30pm",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Doctors Appointment again",
+        day: "Feb 16 at 2:30pm",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Doctors Appointment again again",
+        day: "Feb 17 at 2:30pm",
+        reminder: false,
+    },
+];
