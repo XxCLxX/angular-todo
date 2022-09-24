cf12efa6c258cc47ece7a935283164bbd7550794
Chikondi Luka <65278289+XxCLxX@users.noreply.github.com>
2022-09-24 20:14:54 +0700

    added Delete function

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
-import { TASKS } from 'src/app/mock-tasks';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
 import { Task } from 'src/app/Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiURL = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiURL)
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
